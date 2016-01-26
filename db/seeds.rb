b02f11556f9b4ef4aba9f665ae1cf30bbf60dde3
adrian <adrianzdanowicz@wp.pl>
2016-01-27 00:08:37 +0100

    asd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Book.create(:title => "ksiazka o id 1", :year => "2211", :publisher => "publisher", :genre => "horror", :cober => "asd.jpg", :description => "adas das dasd as das ksiazka o id 1",:rating => 0, :rates => 0, :country => "rosja", :author_id => 2)
-Author.create(:name => "adi", :surname => "gator", :rating => 0, :rates => 0, :country => 'russia', :book_id => 1)
-Book.create(:title => "ksiazka o id 2", :year => "2221", :publisher => "publisher2", :genre => "komedia", :cober => "asd2.jpg", :description => "adas das dasd as das ksiazka o id 1",:rating => 0, :rates => 0, :country => "rosja", :author_id => 1)
-Author.create(:name => "adiaa", :surname => "wwgator", :rating => 0, :rates => 0, :country => 'russia', :book_id => 2)
+
+User.create(:id => 1, :username => "Pan Admin", :email => "admin@mol.pl", :password => "aaaa", :role => "admin")
