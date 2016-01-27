209fa1dd4e7820138d9d83446e611d573e980f3a
adrian <adrianzdanowicz@wp.pl>
2016-01-27 02:53:07 +0100

    fix register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
@@ -58,6 +59,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username,:email, :password, :role)
+    params.require(:user).permit(:id,:username, :email, :password, :role)
   end
 end
