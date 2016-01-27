b714950e04f6a2e5cea13a7e3f1be63663742450
adrian <adrianzdanowicz@wp.pl>
2016-01-27 01:01:42 +0100

    sk

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ActiveRecord::Base
-  searchkick
   has_and_belongs_to_many :books
   has_attached_file  :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
